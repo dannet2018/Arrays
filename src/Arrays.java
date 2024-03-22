d83aef8895fb1fc6a7fcbd6b478b99d19482dbe5
Daniel Hagos <danielwukro@gmail.com>
2024-03-22 12:13:57 -0700

    Initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,26 @@
+
+
+
+
+    public class Arrays {
+        public static void main(String[] args) {
+
+            int[] numbers = new int[3];
+
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+
+            System.out.println("Array elements:");
+            for (int number : numbers) {
+                System.out.println(number);
+            }
+        }
+    }
+
+
+
+
+
